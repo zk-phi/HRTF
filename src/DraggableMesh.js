c676d533df5a689b8348f68df955ce8b41cbffaa
zk-phi <zk-phi@users.noreply.github.com>
2022-03-27 16:44:03 +0900

    Refactor dnd support

---
+++ src/DraggableMesh.js
@@ -0,0 +1,71 @@
+import * as THREE from "three";
+
+export const initDraggable = (renderer, camera, controls) => {
+  let draggableObjects = [];
+  let draggedObject = null;
+
+  const plane = new THREE.Plane();
+  const raycaster = new THREE.Raycaster();
+
+  const mouse = new THREE.Vector2();
+  const updateRaycaster = (e) => {
+    const rect = renderer.domElement.getBoundingClientRect();
+    mouse.x = ((e.clientX - rect.left) / rect.width) * 2 - 1;
+    mouse.y = - ((e.clientY - rect.top) / rect.height) * 2 + 1;
+    raycaster.setFromCamera(mouse, camera);
+  };
+
+  const intersection = new THREE.Vector3();
+  renderer.domElement.addEventListener('mousemove', (e) => {
+    e.preventDefault();
+    if (draggedObject) {
+      updateRaycaster(e);
+      raycaster.ray.intersectPlane(plane, intersection);
+      draggedObject.onDrag(intersection);
+    }
+  }, false);
+
+  renderer.domElement.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    updateRaycaster(e);
+    const intersects = raycaster.intersectObjects(draggableObjects);
+    if (intersects.length > 0) {
+      if (controls) {
+        controls.forEach((control) => control.enabled = false);
+      }
+      draggedObject = intersects[0].object;
+      draggedObject.onDragStart();
+      camera.getWorldDirection(plane.normal);
+    }
+  }, false);
+
+  renderer.domElement.addEventListener('mouseup', (e) => {
+    e.preventDefault();
+    if (controls) {
+      controls.forEach((control) => control.enabled = true);
+    }
+    if (draggedObject) {
+      draggedObject.onDragEnd();
+      draggedObject = null;
+    }
+  }, false);
+
+  class DraggableMesh extends THREE.Mesh {
+    constructor(...props) {
+      super(...props);
+      draggableObjects.push(this);
+    }
+    removeFromParent() {
+      draggableObjects = draggedObject.filter((obj) => obj !== this);
+      super.removeFromParent();
+    }
+    onDragStart() {
+    }
+    onDragEnd() {
+    }
+    onDrag() {
+    }
+  }
+
+  return { DraggableMesh };
+};
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import * as Recorder from "extendable-media-recorder";
 import * as WavEncoder from "extendable-media-recorder-wav-encoder";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { DragControls } from "three/examples/jsm/controls/DragControls.js";
+import { initDraggable } from "./DraggableMesh.js";
 
 const width = 960;
 const height = 640;
@@ -31,6 +32,8 @@ const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(0, 30, 0);
 scene.add(light);
 
+const { DraggableMesh } = initDraggable(renderer, camera, [orbitControls]);
+
 const listener = new THREE.Mesh(
   new THREE.ConeGeometry(0.5, 1, 32),
   new THREE.MeshStandardMaterial({ color: "#ff8888" }),
@@ -39,7 +42,7 @@ listener.position.set(0, 0, 0);
 listener.rotation.set(- Math.PI / 2, 0, 0);
 scene.add(listener);
 
-class Player extends THREE.Mesh {
+class Player extends DraggableMesh {
   constructor (ctx, options) {
     super(
       new THREE.SphereGeometry(0.5),
@@ -60,6 +63,24 @@ class Player extends THREE.Mesh {
     });
     this.splitter.connect(this.gainL, 0).connect(this.panner);
     this.splitter.connect(this.gainR, 1).connect(this.panner);
+    this.previewSource = null;
+  }
+  onDragStart () {
+    this.material.emissive.set(0x333333);
+    this.previewSource = new AudioBufferSourceNode(ctx, { buffer: this.buffer, loop: true });
+    this.previewSource.connect(this.splitter);
+    this.previewSource.start();
+  }
+  onDragEnd () {
+    this.material.emissive.set(0x000000);
+    this.previewSource.stop();
+    this.previewSource = null;
+  }
+  onDrag (position) {
+    this.position.copy(position);
+    this.panner.positionX.value = position.x;
+    this.panner.positionY.value = position.y;
+    this.panner.positionZ.value = position.z;
   }
   setBalance (value /* 0 ~ 1 */) {
     this.gainL.gain.value = (1 - value);
@@ -79,63 +100,7 @@ class Player extends THREE.Mesh {
   disconnect (node) {
     this.panner.disconnect(node);
   }
-  updateAudio () {
-    this.panner.positionX.value = this.position.x;
-    this.panner.positionY.value = this.position.y;
-    this.panner.positionZ.value = this.position.z;
-  }
 }
-
-/**
- * Drag & Drop Mesh
- * - https://irukanobox.blogspot.com/2016/11/threejs3d.html
- * - https://threejs.org/docs/#api/en/core/Raycaster
- */
-
-let draggableObjects = [];
-const plane = new THREE.Plane();
-const raycaster = new THREE.Raycaster();
-let draggedObject = null;
-
-const mouse = new THREE.Vector2();
-const updateRaycaster = (e) => {
-  const rect = renderer.domElement.getBoundingClientRect();
-  mouse.x = ((e.clientX - rect.left) / rect.width) * 2 - 1;
-  mouse.y = - ((e.clientY - rect.top) / rect.height) * 2 + 1;
-  raycaster.setFromCamera(mouse, camera);
-};
-
-const intersection = new THREE.Vector3();
-renderer.domElement.addEventListener('mousemove', (e) => {
-  e.preventDefault();
-  if (draggedObject) {
-    updateRaycaster(e);
-    raycaster.ray.intersectPlane(plane, intersection);
-    draggedObject.position.copy(intersection);
-    draggedObject.updateAudio && draggedObject.updateAudio();
-  }
-}, false);
-
-renderer.domElement.addEventListener('mousedown', (e) => {
-  e.preventDefault();
-  updateRaycaster(e);
-  const intersects = raycaster.intersectObjects(draggableObjects);
-  if (intersects.length > 0) {
-    orbitControls.enabled = false;
-    draggedObject = intersects[0].object;
-    draggedObject.material.emissive.set(0x333333);
-    camera.getWorldDirection(plane.normal);
-  }
-}, false);
-
-renderer.domElement.addEventListener('mouseup', (e) => {
-  e.preventDefault();
-  orbitControls.enabled = true;
-  if (draggedObject) {
-    draggedObject.material.emissive.set(0x000000);
-    draggedObject = null;
-  }
-}, false);
 
 function update () {
   requestAnimationFrame(update);
@@ -173,7 +138,6 @@ async function addAudio (file) {
   if (!ctx) await initialize();
   const player = new Player(ctx, { buffer: await loadFile(ctx, file), loop: false });
   scene.add(player);
-  draggableObjects.push(player);
   player.connect(destination);
   players.push(player);
 }
